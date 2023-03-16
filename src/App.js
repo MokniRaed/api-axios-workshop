6ba710c3e917ca5b792542ebde63612e649d23f3
raedmokni <raedmokni2@gmail.com>
2023-03-16 20:42:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+
+
+
+  //--------- First methode ---------
+  // axios.get("https://jsonplaceholder.typicode.com/users").then((response)=>{
+
+  // setUsers(response.data)
+
+  // }).catch((error)=>{ 
+  //  console.log(error.response.status);
+  // })
+
+//--------- second methode ---------
+  const getMovies = async ()=>{
+    try{
+      const response = await axios.get("https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies")
+      setMovies(response.data)
+    }catch(error){
+      console.log(error.message);
+    }
+  }
+
+
+  
+  useEffect(() => {
+      
+    getMovies()
+    
+    },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {movies.map((ele) => <div><h2>{ele.Title}</h2> <img alt={ele.Title} src={ele.image}/></div>)}
     </div>
   );
 }
